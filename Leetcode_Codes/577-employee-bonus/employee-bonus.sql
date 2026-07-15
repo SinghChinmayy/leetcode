37e9e054ec3bc2f47bf990c88ec541c78933a30f
Chinmay Singh <136782221+SinghChinmayy@users.noreply.github.com>
2026-07-15 15:24:53 +0530

    Time: 286 ms (99.52%) | Memory: 0B (100.00%) - LeetSync

---
+++ Leetcode_Codes/577-employee-bonus/employee-bonus.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+select e.name as name, b.bonus as bonus
+from Employee as e
+left join bonus as b
+on e.empId = b.empId
+where b.bonus < 1000
+or b.bonus is null;
