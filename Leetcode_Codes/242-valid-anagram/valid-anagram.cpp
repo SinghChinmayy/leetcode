26cfc524b9086c339c406ec17029aec6426419af
Chinmay Singh <136782221+SinghChinmayy@users.noreply.github.com>
2026-08-09 05:22:01 +0530

    Time: 7 ms (18.34%) | Memory: 9.7 MB (76.49%) - LeetSync

--- Leetcode_Codes/242-valid-anagram/valid-anagram.cpp
+++ Leetcode_Codes/242-valid-anagram/valid-anagram.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-    //    insert s in hash table/unordered map
-    unordered_map<char, int> chars;
-    for(char c : s){
-        chars[c] += 1;        
-    }
-    // validate t with s 
-    for(char c : t){
-        chars[c] -= 1;
-    }
-    for(auto[key, value] : chars){
-        if(value != 0){
-            return false;
-        }
-    }
-    return true;
+       if(s.length() != t.length()) return false;
+       sort(s.begin(), s.end());
+       sort(t.begin(), t.end());
+       if (s == t) return true;
+       return false;
     }
 };
